5a7caee462a4ed029e43f6dc289673c245d8f059
JhonnyFlores <Jhonny.fl2006@gmail.com>
2026-01-28 10:05:24 -0600

    Subida de Hola mundo en c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world!" << endl;
+    cout << "Jhonny Flores" << endl;
+    cout << "9959-25-4900" << endl;
+    return 0;
+}
